ee501547a6707fcbbe069e1794aad2993c09a5b4
cbatsoula <cbatsoula@gmail.com>
2019-07-24 11:06:16 -0400

    tried doing the thing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,43 @@ import Bookshelf from "./containers/Bookshelf";
 class App extends Component {
   state = {
     books: [],
+    author: "",
+    title: "",
+    img: "",
+    newBook: null,
+    // newBooks: []
+  }
+    //last 15 min mark, thinking about lifting state so I can add these values to a newBook object in App and there I have my books [] in state which I could use to setState with something like this.setState({ books: [...books, {newBook}] })
+
+  handleChange = (event) => {
+    // console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  //this doesnt work yet
+  handleSubmit = (event) => {
+    event.persist();
+    console.log("submit!!", this.state)
+    if (!this.state.newBook) {
+      let bookInfo = { title: this.state.title, author: this.state.author, img: this.state.img }
+      debugger;
+        this.setState({
+          books: [...this.state.books, {bookInfo}]
+        })
+        return this.state.newBook
+    } else {
+      return this.state.newBook
+    }
+      // debugger;
+      //page instantly reloads on submit and my state is wiped
+      // this.setState({
+      //   author: "",
+      //   title: "",
+      //   img: ""
+      // })
+    debugger;
   }
 
   componentDidMount() {
@@ -43,12 +80,19 @@ class App extends Component {
 
 
   render() {
-    console.log(this.state.books)
+    console.log(this.state)
     return (
       <div className="book-container">
         <BookList
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          title={this.state.title}
+          img={this.state.img}
+          author={this.state.author}
+
           handleBookClick={this.handleBookClick}
           books={this.state.books}/>
+
         <Bookshelf
           handleBookClick={this.handleBookClick}
           books={this.filterBooks()}/>
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,57 +3,59 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
-  state = {
-    author: "",
-    title: "",
-    img: "",
-    newBook: null,
-    // newBooks: []
-  }
-
-  handleChange = (event) => {
-    // console.log(event.target.value)
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  handleSubmit = (event) => {
-    event.persist();
-    console.log("submit!!", this.state)
-    if (!this.state.newBook) {
-      let bookInfo = { title: this.state.title, author: this.state.author, img: this.state.img }
-      debugger;
-        this.setState({
-          newBook: bookInfo
-        })
-        return this.state.newBook
-    } else {
-      return this.state.newBook
-    }
-      // debugger;
-      //page instantly reloads on submit and my state is wiped
-      // this.setState({
-      //   author: "",
-      //   title: "",
-      //   img: ""
-      // })
-    debugger;
-  }
+  // state = {
+  //   author: "",
+  //   title: "",
+  //   img: "",
+  //   newBook: null,
+  //   // newBooks: []
+  // }
+  //
+  // handleChange = (event) => {
+  //   // console.log(event.target.value)
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   })
+  // }
+  //
+  // handleSubmit = (event) => {
+  //   event.persist();
+  //   console.log("submit!!", this.state)
+  //   if (!this.state.newBook) {
+  //     let bookInfo = { title: this.state.title, author: this.state.author, img: this.state.img }
+  //     debugger;
+  //       this.setState({
+  //         newBook: bookInfo
+  //       })
+  //       return this.state.newBook
+  //   } else {
+  //     return this.state.newBook
+  //   }
+  //     // debugger;
+  //     //page instantly reloads on submit and my state is wiped
+  //     // this.setState({
+  //     //   author: "",
+  //     //   title: "",
+  //     //   img: ""
+  //     // })
+  //   debugger;
+  // }
   //i have my form set up, on submit the book needs to show up on the bookList
   //make another array for newly made books and use conditional rendering
   //last 15 min mark, thinking about lifting state so I can add these values to a newBook object in App and there I have my books [] in state which I could use to setState with something like this.setState({ books: [...books, {newBook}] })
 
 
   render() {
-    console.log("book list", this.state)
+    // console.log("book list", this.state)
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
-          author={this.state.author}/>
+          handleSubmit={this.props.handleSubmit}
+          handleChange={this.props.handleChange}
+          title={this.props.title}
+          img={this.props.img}
+          author={this.props.author}/>
         <ul>
           {this.props.books.map(book => {
             return <Book handleBookClick={this.props.handleBookClick} book={book} key={book.id}/>
